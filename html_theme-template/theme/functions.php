2ac3fdd28d8f32ef164da586780c20da75264079
Nikhar Pandya <70164257+NikharPandya@users.noreply.github.com>
2022-08-09 16:07:55 -0400

    added files

---
+++ html_theme-template/theme/functions.php
@@ -0,0 +1,22 @@
+<?php 
+
+function classic_ace_register_styles(){
+
+    $version = wp_get_theme()-> get('Version');
+
+    wp_enqueue_style('classicace-custom_style', get_template_directory_uri() . "/style,css", array('classicace-bootstrap'), $version, 'all');
+    wp_enqueue_style('classicace-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css", array(), '5.2.0', 'all');
+
+}
+
+add_action('wp_enqueue_scripts', 'classic_ace_register_styles');
+
+function classic_ace_register_scripts(){
+    
+    wp_enqueue_script('classicace-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js", array(), '5.2.0');
+    
+}
+
+add_action('wp_enqueue_scripts', 'classic_ace_register_scripts');
+
+?>
