f7c4b83ac2621547928daf34a7e03989ef266b14
Nikhar Pandya <70164257+NikharPandya@users.noreply.github.com>
2022-10-15 14:24:51 +0000

    added header and footer

---
+++ html_theme-template/theme/footer.php
@@ -0,0 +1,45 @@
+<footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 px-5 mx-0 text-danger bg-dark w-100 mt-auto">
+          <div class="col mb-3">
+            <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+              <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+            </a>
+            <p class="text-muted">&copy; 2022</p>
+          </div>
+      
+          <div class="col mb-3">
+      
+          </div>
+      
+          <div class="col mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Home</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Features</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Pricing</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">FAQs</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">About</a></li>
+            </ul>
+          </div>
+      
+          <div class="col mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Home</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Features</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Pricing</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">FAQs</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">About</a></li>
+            </ul>
+          </div>
+      
+          <div class="col mb-3">
+              <h5>Section</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Features</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">Pricing</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-danger">About</a></li>
+              </ul>
+            </div>
+      </footer>
---
+++ html_theme-template/theme/header.php
@@ -0,0 +1,14 @@
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom px-5">
+              <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <span class="fs-4">Site Title</span>
+              </a>
+              <ul class="nav nav-pills">
+                <li class="nav-item"><a href="./front-page.html" class="nav-link" aria-current="page">Front page</a></li>
+                <li class="nav-item"><a href="#" class="nav-link active">Blog</a></li>
+                <li class="nav-item"><a href="./page.html" class="nav-link">Page</a></li>
+                <li class="nav-item"><a href="./single.html" class="nav-link">Single Post</a></li>
+                <li class="nav-item"><a href="./archive.html" class="nav-link">Archive</a></li>
+                <li class="nav-item"><a href="./search.html" class="nav-link">Search</a></li>
+                <li class="nav-item"><a href="./404.html" class="nav-link">404</a></li>
+              </ul>
+        </header>
